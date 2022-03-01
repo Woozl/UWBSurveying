9c5ae46053e7185811540eb9352f1277ec52a057
David Glymph <Woozl@users.noreply.github.com>
2022-02-28 21:06:03 -0500

    USB MSC using .bss buffer. Plug cable into USB FS port to format and open storage. Only retains data when powered.

--- STM32CubeIDE/CM7/Application/User/Core/usb/usbd_storage_if.c
+++ STM32CubeIDE/CM7/Application/User/Core/usb/usbd_storage_if.c
@@ -63,10 +63,12 @@
   */
 
 #define STORAGE_LUN_NBR                  1
-#define STORAGE_BLK_NBR                  0x10000
+#define STORAGE_BLK_NBR                  200
 #define STORAGE_BLK_SIZ                  0x200
 
 /* USER CODE BEGIN PRIVATE_DEFINES */
+
+uint8_t buffer[STORAGE_BLK_NBR * STORAGE_BLK_SIZ]; // 128 * 512 bits = 8kb buffer
 
 /* USER CODE END PRIVATE_DEFINES */
 
@@ -229,6 +231,7 @@ int8_t STORAGE_IsWriteProtected_FS(uint8_t lun)
 int8_t STORAGE_Read_FS(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t blk_len)
 {
   /* USER CODE BEGIN 6 */
+	memcpy(buf, &buffer[blk_addr*STORAGE_BLK_SIZ], blk_len*STORAGE_BLK_SIZ);
   return (USBD_OK);
   /* USER CODE END 6 */
 }
@@ -241,6 +244,7 @@ int8_t STORAGE_Read_FS(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t bl
 int8_t STORAGE_Write_FS(uint8_t lun, uint8_t *buf, uint32_t blk_addr, uint16_t blk_len)
 {
   /* USER CODE BEGIN 7 */
+	memcpy(&buffer[blk_addr*STORAGE_BLK_SIZ], buf, blk_len*STORAGE_BLK_SIZ);
   return (USBD_OK);
   /* USER CODE END 7 */
 }
